1043943b75a4d6e87338a505f0b04b51274d7eed
Justhasim <thatshasim@gmail.com>
2024-03-12 16:41:27 +0530

    add routes and created some component

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './component/Home'
+import About from './component/About'
+import Signin from './component/Signin'
+import Signup from './component/Signup'
+import Profile from './component/Profile'
 
 export default function App() {
   return (
-    <h1 className='text-green-400'> hello...</h1>
+    <>
+ <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/sign-in' element={<Signin/>}  />
+    <Route path='/sign-up' element={<Signup/>}  />
+    <Route path='profile' element={<Profile/>}  />
+
+   </Routes>
+   </BrowserRouter>
+    </>
 
   )
 }
